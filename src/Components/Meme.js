536ad4ed04ad3048285686dca3c00431cf79c883
Afsoon Taghizadehgan <tagh0015@algonquinlive.com>
2023-02-09 14:40:11 -0500

    changed state variable to be an object, memData

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -3,21 +3,36 @@ import Data from '../memesData.js'
 import React from 'react'
 
 export default function Meme(){
-    const [ memeUrl, setMemeUrl]=React.useState("")
+    const [ memeData, setMemeData]=React.useState({
+        memeTop:"",
+        memeUrl:"",
+        memeBottom:""
+    })
+
+    const [allMemeImages, setAllMemeImages]= React.useState(Data)
+
 
     function memePick(){
-        const memesArrayLength= Data.data.memes.length
+        const memesArrayLength= allMemeImages.data.memes.length
         const number= Math.floor(Math.random() * (memesArrayLength+1));
-        setMemeUrl(Data.data.memes[number].url)
+        setMemeData( preveState => ({
+            ...preveState,
+            memeUrl: allMemeImages.data.memes[number].url})
+        
+        )
     }
    
     return(
         <main className='meme'>
             <div className='meme--form'>
-                <input type="text" className="form--input" placeholder='Top Text'/>
+                <input type="text" className="form--input" placeholder='Top Text' />
                 <input type="text" className="form--input" placeholder='Bottom Text'/> 
-                <button className='meme--button' type="button" onClick={memePick}>Get a new meme image  🖼</button>
-                <img src={memeUrl} className='meme--img'/>
+                <button className='meme--button' type="button" onClick={memePick}>Get a new meme image  🖼</button> 
+            </div>
+            <div className='meme--package'>
+                <h3 className='img--top'>{memeData.memeTop}</h3>
+                <img src={memeData.memeUrl} className='meme--img' alt=""/>
+                <h3 className='img--bottom'>{memeData.memeBottom}</h3>
             </div>
             
         </main>

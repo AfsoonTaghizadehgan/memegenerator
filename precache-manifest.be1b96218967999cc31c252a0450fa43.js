f15aa07fd54fe3caa90755c2adfd21f2f01bf735
Afsoon Taghizadehgan <tagh0015@algonquinlive.com>
2023-04-04 09:20:06 -0400

    Updates

---
+++ precache-manifest.be1b96218967999cc31c252a0450fa43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8d4556931924b05eaca",
+    "url": "/memegenerator/static/css/main.0ef59feb.chunk.css"
+  },
+  {
+    "revision": "f8d4556931924b05eaca",
+    "url": "/memegenerator/static/js/main.f8d45569.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/memegenerator/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "316e54d8786d6e2718b0",
+    "url": "/memegenerator/static/js/2.316e54d8.chunk.js"
+  },
+  {
+    "revision": "f5ed166e35aee08a2b80",
+    "url": "/memegenerator/static/js/runtime~main.f5ed166e.js"
+  },
+  {
+    "revision": "8440e3541f6174bf2a1dd9063b97b8ac",
+    "url": "/memegenerator/index.html"
+  }
+];
